9ab5a79c0078eed3bf3fa142c79e475a9f136245
Bhargav_Sharma <sreebhargava.kandala@gmail.com>
2025-06-30 18:28:56 +0530

    Update first_day.cpp

--- first_day.cpp
+++ first_day.cpp
@@ -4,10 +4,12 @@ using namespace std;
 
 int main() {
     
-    int x, y;
+    long long int x, y;
     cin >> x >> y;
 
-    cout << x << " + " << y << " = " << x+y;
+    cout << x << " + " << y << " = " << x+y << endl;
+    cout << x << " * " << y << " = " << x * y << endl;
+    cout << x << " - " << y << " = " << x - y << endl;
 
     return 0; 
 }
